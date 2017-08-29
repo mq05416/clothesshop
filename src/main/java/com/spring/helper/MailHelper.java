4b59ccd7ba58a8fdd62d40ee5522e69cad94469e
luan711 <luannv.aptech2015@gmail.com>
2017-08-29 16:46:20 +0700

    last version

--- src/main/java/com/spring/helper/MailHelper.java
+++ src/main/java/com/spring/helper/MailHelper.java
@@ -37,51 +37,6 @@ public class MailHelper {
 		
 		
 		
-		/*String result;
-		   // Recipient's email ID needs to be mentioned.
-		   //String to = "abcd@gmail.com";
-
-		   // Sender's email ID needs to be mentioned
-		   String from = "luannv.aptech2015@gmail.com";
-
-		   // Assuming you are sending email from localhost
-		   String host = "localhost";
-
-		   // Get system properties object
-		   Properties properties = System.getProperties();
-
-		   // Setup mail server
-		   properties.setProperty("mail.smtp.auth", "true");
-		   properties.setProperty("mail.smtp.starttls.enable", "true");
-		   properties.setProperty("mail.smtp.host", "smtp.gmail.com");
-		   properties.setProperty("mail.smtp.port", "587");
-		   properties.setProperty("mail.smtp.host", host);
-
-		   // Get the default Session object.
-		   Session mailSession = Session.getDefaultInstance(properties);
-
-		   try{
-		      // Create a default MimeMessage object.
-		      MimeMessage message = new MimeMessage(mailSession);
-		      // Set From: header field of the header.
-		      message.setFrom(new InternetAddress(from));
-		      // Set To: header field of the header.
-		      message.addRecipient(Message.RecipientType.TO,
-		                               new InternetAddress(to));
-		      // Set Subject: header field
-		      message.setSubject("This is the Subject Line!");
-		     
-		      // Send the actual HTML message, as big as you like
-		      message.setContent("<h1>This is actual message</h1>",
-		                            "text/html" );
-		      // Send message
-		      Transport.send(message);
-		      result = "Sent message successfully....";
-		      return true;
-		   }catch (MessagingException mex) {
-		      mex.printStackTrace();
-		      result = "Error: unable to send message....";
-		      return false;
-		   }*/
+		
 	}
 }
